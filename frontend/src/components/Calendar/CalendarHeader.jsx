123d3187202f3d426e2924330ac6924c9b1667ed
petrova-olena <petrova.olena.anatoliivna@gmail.com>
2026-04-20 13:22:01 +0300

    Add switching between months

--- frontend/src/components/Calendar/CalendarHeader.jsx
+++ frontend/src/components/Calendar/CalendarHeader.jsx
@@ -7,11 +7,15 @@ export default function CalendarHeader({
 }) {
   return (
     <div className="month">
-      <i className="fas fa-angle-left prev" onClick={prevMonth}></i>
+      <i className="prev" onClick={prevMonth}>
+        ←
+      </i>
       <div className="date">
         {months[month]} {year}
       </div>
-      <i className="fas fa-angle-right next" onClick={nextMonth}></i>
+      <i className="next" onClick={nextMonth}>
+        →
+      </i>
     </div>
   );
 }
